a519c448299dd76bbd6dd32dee22669a3f33b978
Dreuyasha <drekhalifa2017@gmail.com>
2025-10-19 12:03:36 -0400

    still trying 8081

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const app = express();
-const port = 8080;
+const port = 8081;
 
 const db = mysql.createConnection({
   host: "db",
